7bc994417966975a91dacb72a78462dc3f1db9b2
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2022-03-16 19:39:25 +0200

    aggregations exercises

--- course/aggregations.py
+++ course/aggregations.py
@@ -66,13 +66,50 @@ best_movies_df = movies_df.orderBy(col("IMDB_Rating").desc())
 proper_worst_movies_df = movies_df.orderBy(col("IMDB_Rating").asc_nulls_last())
 
 """
-Exercises
-    1. Sum up ALL the profits of ALL the movies in the dataset
-    2. Count how many distinct directors we have
-    3. Show the mean+stddev for US gross revenue
-    4. Compute the average IMDB rating + average US gross PER DIRECTOR
-    5. Show the average difference between IMDB rating and Rotten Tomatoes rating
+    Exercises
+        1. Sum up ALL the profits of ALL the movies in the dataset
+        2. Count how many distinct directors we have
+        3. Show the mean+stddev for US gross revenue
+        4. Compute the average IMDB rating + average US gross PER DIRECTOR
+        5. Show the average difference between IMDB rating and Rotten Tomatoes rating
 """
 
+# 1
+# careful with nulls
+all_profits_df = movies_df.selectExpr("sum(Worldwide_Gross + US_DVD_Sales - Production_Budget) as Total_Profit")
+all_profits_df_v2 = movies_df.selectExpr("sum(Worldwide_Gross) + sum(US_DVD_Sales) - sum(Production_Budget) as Total_Profit")
+
+# 2
+unique_director_count_df = movies_df.select(countDistinct(col("Director")))
+dist_director_df = movies_df.selectExpr("count(Distinct Director)")
+
+# 3
+US_Gross_stats_df = movies_df.select(
+    mean(col("US_Gross")),
+    stddev(col("US_Gross"))
+)
+
+# 4
+aggregations_by_director_df_lai = movies_df.\
+    groupBy(col("Director")).\
+    agg(
+        # use strings here for column names
+        avg("US_Gross").alias("Avg_US_Gross"),
+        avg("IMDB_Rating").alias("Avg_Rating")
+    )
+
+# 5
+# normalize the columns compared
+rotten_imdb_avg_diff_jackson = movies_df.\
+    select((avg(col("IMDB_rating") * 10 - col("Rotten_Tomatoes_Rating"))).\
+           alias("Avg_IMDB_RT_Ppt_Diff"))
+
+# careful of avg(difference) vs difference of avgs
+avg_diff_two_ratings_df = movies_df.agg(
+        avg("IMDB_Rating").alias("Avg_IMDB")
+        ,avg("Rotten_Tomatoes_Rating").alias("Avg_Rotten")
+).withColumn("Ratings_diff", col("Avg_IMDB") * 10 - col("Avg_Rotten"))
+
 if __name__ == '__main__':
-    aggregations_by_genre_df.show()
+    all_profits_df.show()
+    all_profits_df_v2.show()
